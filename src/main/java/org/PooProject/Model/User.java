f76123b91e076dc959d49ff72d2a1ca2734ba933
Marcos_Henrique_710 <marcoshenriqueufrn@gmail.com>
2025-11-13 09:22:19 -0300

    Starting the code.

--- src/main/java/org/PooProject/Model/User.java
+++ src/main/java/org/PooProject/Model/User.java
@@ -1,4 +1,37 @@
 package org.PooProject.Model;
 
 public class User {
+    private String nome;
+    private String matricula;
+    private String cpf;
+
+    public User (String nome, String matricula, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.matricula = matricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
